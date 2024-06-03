add1ce792f4521b4743ec7941a68bee065c37870
lord-cosmos <teslaatozinspires7@gmail.com>
2024-06-03 11:35:14 -0400

    pre-alpha push

---
+++ Assets/Scripts/ShootProjectile.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShootProjectile : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 1000f;
+    GameObject currentProjectilePrefab;
+
+    void Start()
+    {
+        currentProjectilePrefab = projectilePrefab;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject projectile = Instantiate(currentProjectilePrefab, transform.position + transform.forward, transform.rotation) as GameObject;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+    }
+
+}
